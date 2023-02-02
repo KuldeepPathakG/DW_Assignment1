be6f55d1d01d5cbbd14beee9e39adde86b0a5bea
KuldeepPathakG <101445286+KuldeepPathakG@users.noreply.github.com>
2023-02-02 22:10:10 +0530

    Add files via upload

---
+++ DW_Assignment1.py
@@ -0,0 +1,26 @@
+#Question1:
+import numpy as np
+a= np.zeroe(10)
+a[4]=1
+print(a)
+
+#Question2:
+np.arange(10,50)
+
+#Question3:
+np.arange(0,9).reshape(3,3)
+
+#Question4:
+b=np.array([1,2,0,0,4,0])
+np.nonzero(a)
+
+#Question5:
+c=np.random.random((10,10))
+print(c)
+print(c.min())
+print(c.max())
+
+#Question6:
+d=np.random.rand(30)
+print(d)
+print("Mean Value: ",d.mean())
